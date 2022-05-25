32368c2ad39371db14e59def162991d3c9dfb94c
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-05-25 14:47:15 +0900

    공통 툴 레이어 작성

---
+++ battleground/Assets/1.Scripts/Tool/Editor/EffectTool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Text;
+using UnityObject = UnityEngine.Object;
+
+/// <summary>
+/// 
+/// </summary>
+public class EffectTool : EditorWindow
+{
+    //UI 그리는데 필요한 변수들
+    public int uiWidthLarge = 300;
+    public int uiWidthMiddle = 200;
+    private int selection = 0;
+    private Vector2 SP1 = Vector2.zero;
+    private Vector2 SP2 = Vector2.zero;
+}
