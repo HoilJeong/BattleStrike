3512bdfa46ee61c69b81de4661972ec15c8cca0e
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2022-05-26 21:57:28 +0900

    사운드 매니저1

---
+++ battleground/Assets/1.Scripts/Manager/SoundManager.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : SingletonMonobehaviour<SoundManager>
+{
+    public const string MasterGroupName = "Master";
+    public const string EffectGroupName = "Effect";
+    public const string BGMGroupName = "BGM";
+    public const string UIGroupName = "UI";
+    public const string MixerName = "AudioMixer";
+    public const string ContainerName = "SoundContainer";
+    public const string FadeA = "FadeA";
+    public const string FadeB = "FadeB";
+    public const string UI = "UI";
+    public const string EffectVolumeParam = "Volume_Effect";
+    public const string BGMVolumeParam = "Volume_BGM";
+    public const string UIVolumeParam = "Volume_UI";
+
+    public enum MusicPlayingType
+    { 
+        None = 0,
+        SourceA = 1,
+        SourceB = 2,
+        AtoB = 3,
+        BtoA = 4,
+    }
+
+    public AudioMixer mixer = null;
+    public Transform audioRoot = null;
+    public AudioSource fadeA_audio = null;
+    public AudioSource fadeB_audio = null;
+    public AudioSource[] effect_audios = null;
+    public AudioSource UI_audio = null;
+
+    public float[] effect_PlayStartTime = null;
+    private int EffectChannelCount = 5;
+    private MusicPlayingType currentPlayingType = MusicPlayingType.None;
+    private bool isTicking = false;
+    private SoundClip currentSound = null;
+    private SoundClip lastSound = null;
+    private float minVolume = -80.0f;
+    private float maxVolume = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.mixer == null)
+        {
+            this.mixer = Resources.Load(MixerName) as AudioMixer;
+        }
+        if (this.audioRoot == null)
+        {
+            audioRoot = new GameObject(ContainerName).transform;
+            audioRoot.SetParent(transform);
+            audioRoot.localPosition = Vector3.zero;
+        }
+        if (fadeA_audio == null)
+        {
+            GameObject fadeA = new GameObject(FadeA, typeof(AudioSource));
+            fadeA.transform.SetParent(audioRoot);
+            this.fadeA_audio = fadeA.GetComponent<AudioSource>();
+            this.fadeA_audio.playOnAwake = false;
+        }
+        if (fadeB_audio == null)
+        {
+            GameObject fadeB = new GameObject(FadeB, typeof(AudioSource));
+            fadeB.transform.SetParent(audioRoot);
+            fadeB_audio = fadeB.GetComponent<AudioSource>();
+            fadeB_audio.playOnAwake = false;
+        }
+        if (UI_audio == null)
+        {
+            GameObject ui = new GameObject(UI, typeof(AudioSource));
+            ui.transform.SetParent(audioRoot);
+            UI_audio = ui.GetComponent<AudioSource>();
+            UI_audio.playOnAwake = false;
+        }
+        if (this.effect_audios == null || this.effect_audios.Length == 0)
+        {
+            this.effect_PlayStartTime = new float[EffectChannelCount];
+            this.effect_audios = new AudioSource[EffectChannelCount];
+            for (int i = 0; i < EffectChannelCount; i++)
+            {
+                effect_PlayStartTime[i] = 0.0f;
+                GameObject effect = new GameObject("Effect" + i.ToString(), typeof(AudioSource));
+                effect.transform.SetParent(audioRoot);
+                this.effect_audios[i] = effect.GetComponent<AudioSource>();
+                this.effect_audios[i].playOnAwake = false;
+            }
+        }
+
+        if (this.mixer != null)
+        {
+            this.fadeA_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(BGMVolumeParam)[0];
+            this.fadeB_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(BGMVolumeParam)[0];
+            this.UI_audio.outputAudioMixerGroup = mixer.FindMatchingGroups(UIGroupName)[0];
+            for (int i = 0; i < this.effect_audios.Length; i++)
+            {
+                this.effect_audios[i].outputAudioMixerGroup = mixer.FindMatchingGroups(EffectGroupName)[0];
+            }
+        }
+
+        VolumeInit();
+    }
+
+    public void SetBGMVolume(float currentRatio)
+    {
+        currentRatio = Mathf.Clamp01(currentRatio);
+        float volume = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        this.mixer.SetFloat(BGMVolumeParam, volume);
+        PlayerPrefs.SetFloat(BGMVolumeParam, volume);
+    }
+
+    public float GetBGMVolume()
+    {
+        if (PlayerPrefs.HasKey(BGMVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(BGMVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
+    }
+
+    public void SetEffectVolume(float currentRatio)
+    {
+        currentRatio = Mathf.Clamp01(currentRatio);
+        float volume = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        this.mixer.SetFloat(EffectVolumeParam, volume);
+        PlayerPrefs.SetFloat(EffectVolumeParam, volume);
+    }
+
+    public float GetEffectVolume()
+    {
+        if (PlayerPrefs.HasKey(EffectVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(EffectVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
+    }
+
+    public void SetUIVolume(float currentRatio)
+    {
+        currentRatio = Mathf.Clamp01(currentRatio);
+        float volume = Mathf.Lerp(minVolume, maxVolume, currentRatio);
+        this.mixer.SetFloat(UIVolumeParam, volume);
+        PlayerPrefs.SetFloat(UIVolumeParam, volume);
+    }
+
+    public float GetUIVolume()
+    {
+        if (PlayerPrefs.HasKey(UIVolumeParam))
+        {
+            return Mathf.Lerp(minVolume, maxVolume, PlayerPrefs.GetFloat(UIVolumeParam));
+        }
+        else
+        {
+            return maxVolume;
+        }
+    }
+
+    void VolumeInit()
+    {
+        if (this.mixer != null)
+        {
+            this.mixer.SetFloat(BGMVolumeParam, GetBGMVolume());
+            this.mixer.SetFloat(EffectVolumeParam, GetEffectVolume());
+            this.mixer.SetFloat(UIVolumeParam, GetUIVolume());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
